bd64043708c4061b148969d6513b52121353780f
L D <ldeunert@gmail.com>
2021-01-04 00:58:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function addToCart(item) {
 
 function viewCart() {
   if (cart.length === 0) {
-    return `Your shopping cart is empty`
+    return `Your shopping cart is empty.`
   }
   else {
     cartItems = []
